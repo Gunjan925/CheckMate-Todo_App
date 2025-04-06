f7b2f0848ed7578785dc39b93aff7ecb3283f8c4
Gunjan925 <innanigunjan@gmail.com>
2025-04-06 09:39:20 +0530

    Adding signup , login page and the backend folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUp from './Components/signup';
+import Login from './Components/login';
+import AddTodo from './Components/addTodo';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/addTodo" element={<AddTodo />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/Components/addTodo.js
@@ -0,0 +1,12 @@
+import React , { useState  } from "react";
+import '../Style/addTodo.css';
+
+function AddTodo () {
+    return(
+        <>
+            <h1>Add Todo</h1>
+        </>
+    );
+}
+
+export default AddTodo;
